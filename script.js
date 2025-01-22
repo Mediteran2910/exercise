5cff4c48a81c0d669a2af32bccfa74b97c09bcb9
Mediteran2910 <marindonadini00@gmail.com>
2025-01-22 18:56:15 +0100

    improving seo

--- script.js
+++ script.js
@@ -8,6 +8,33 @@ burgerIcon.addEventListener("click", () => {
 });
 
 closeButton.addEventListener("click", () => {
-  menu.classList.remove("active");
   menu.classList.remove("scale-in-tr");
+  menu.classList.add("scale-out-tr");
+
+  setTimeout(() => {
+    menu.classList.remove("active");
+    menu.classList.remove("scale-out-tr");
+  }, 300);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedServicesLogos = document.querySelectorAll(
+    ".one-technologies-wrap"
+  );
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("bounce-in-fwd");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.6,
+    }
+  );
+
+  animatedServicesLogos.forEach((element) => observer.observe(element));
 });
